7a26ee289b2c247b9dcde46cf3373aed1e85e73d
Harry Weiss <harrisonrweiss1@gmail.com>
2023-11-07 10:58:19 -0500

    Working -> need to adjust so it can report forecasts for each category of transportation.

--- avidxchange_data_analysis/analytics_test.py
+++ avidxchange_data_analysis/analytics_test.py
@@ -12,7 +12,7 @@ def ask_for_file():
     # Show an "Open" dialog box and return the path to the selected file
     file_path = filedialog.askopenfilename(
         title="Select the Excel file",
-        filetypes=(("Excel files", "*.xlsx;*.xls"), ("All files", "*.*"))  # Optional: specify file types
+        # filetypes=(("Excel files", "*.xlsx;*.xls"), ("All files", "*.*"))  # Optional: specify file types
     )
     
     return file_path
@@ -21,7 +21,20 @@ def ask_for_file():
 excel_file_path = ask_for_file()
 
 # Data Preprocessing
-df = pd.read_excel(excel_file_path)
+# Read the entire Excel sheet
+df_full = pd.read_excel(excel_file_path)
+
+# Find the first row index where all columns have non-NaN values
+start_row = None
+for i in range(len(df_full)):
+    # Assuming the actual data has fewer NaNs than the header
+    if df_full.iloc[i].notnull().sum() > 10:  # 'threshold' is how many non-NaNs you expect
+        start_row = i
+        break
+
+# Now read the Excel file starting from the detected row and set header row to the first row
+df = pd.read_excel(excel_file_path, skiprows=start_row, header=1)
+df.columns = df.columns.str.strip()
 
 # Convert 'Revenue Month' to datetime
 df['Revenue Month'] = pd.to_datetime(df['Revenue Month'])
@@ -40,8 +53,34 @@ model.fit(df_prophet)
 future = model.make_future_dataframe(periods=365, freq='D')
 forecast = model.predict(future)
 
-# Save the forecast to an Excel file
-forecast_output_path = filedialog.asksaveasfilename # filedialog.asksaveasfilename to get path from user
-forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_excel(forecast_output_path, index=False)
+# Save file features
+def ask_save_as_file():
+    # Create a root window, but don't display it
+    root = tk.Tk()
+    root.withdraw()  # We don't want a full GUI, so keep the root window from appearing
 
-print(f"The forecast has been saved to {forecast_output_path}")
+    # Show a "Save As" dialog box and return the path to the selected file
+    file_path = filedialog.asksaveasfilename(
+        title="Save the Excel file",
+        filetypes=(("Excel files", "*.xlsx"), ("All files", "*.*"))  # Optional: specify file types
+    )
+    
+    # Destroy the root window after use
+    root.destroy()
+
+    return file_path
+
+# Use the function to get the file path
+excel_save_path = ask_save_as_file()
+
+# If the user cancels, the function will return '', so check if a path was provided
+if excel_save_path:
+    # Continue with saving the file using the provided path
+    # ... your code to save the file goes here
+    print(f"File will be saved to: {excel_save_path}")
+else:
+    print("File save operation was canceled.")
+
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_excel(excel_save_path, index=False)
+
+print(f"The forecast has been saved to {excel_save_path}")
